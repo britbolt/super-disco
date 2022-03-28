729e1d4425e7ae906ed4382f1eca604f7f91d7a7
britbolt <blattisaw@gmail.com>
2022-03-27 23:32:20 -0400

    working on date compare

--- develop/schedule.js
+++ develop/schedule.js
@@ -1,2 +1,37 @@
-var today = moment().calendar();
-document.getElementById("currentDate").innerHTML = today;
+const DateTime = luxon.DateTime;
+
+const dt = DateTime.now();
+
+let now = DateTime.local().toFormat("h");
+
+//display current date in header
+window.onload = function showDate () {
+    document.getElementById('currentDay').innerHTML = dt.toLocaleString(DateTime.DATETIME_MED) ;
+   
+};
+console.log(now);
+
+const rows = document.getElementsByClassName("row");
+
+Array.from(rows).forEach(rows => {
+let scheduleTime = rows.id, timeHours;
+if (scheduleTime) {
+timeHours = parseInt(scheduleTime);
+}
+//console.log(timeHours);
+ 
+if (timeHours){
+  if (timeHours === now){
+  rows.style.backgroundColor = "blue";
+}
+  else if (now < timeHours) {
+  rows.style.backgroundColor = "green";
+}
+  else if (now > timeHours) {
+  rows.style.backgroundColor = "red";
+}
+};  
+});
+
+//update event when textarea is used
+ 
